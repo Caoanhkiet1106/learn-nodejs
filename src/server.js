4665f18e8c60913ba560752c4a730d93db7ca5b0
Anh Kiet <Caoanhkiet753@gmail.com>
2024-08-18 11:10:45 +0700

    Install perttier, lint-satged, husky

--- src/server.js
+++ src/server.js
@@ -2,25 +2,25 @@ import { engine } from 'express-handlebars';
 import express from 'express';
 import routes from './routes/index.js';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-
-app.use(express.static('src/public'))
-app.use(express.urlencoded({
-    extended:true
-}));
+app.use(express.static('src/public'));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // template enginee
 app.engine('hbs', engine());
 app.set('view engine', 'hbs');
 app.set('views', 'src/resources/views');
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 
-
-routes(app)
+routes(app);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
